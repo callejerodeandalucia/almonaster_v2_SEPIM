59b1a1dd6febf119a3c04b3e1f33762dd23cec81
callejerodeandalucia <cdau.ieca@juntadeandalucia.es>
2017-05-19 13:36:21 +0200

    Update index.js

--- js/index.js
+++ js/index.js
@@ -75,7 +75,7 @@ function geolocalizar(){
 			  cargarCategoria();
 		  };
 		  var errorFunction = function(){
-			  alert("Compruebe que tiene activada la ubicación del dispositivo y que se encuentra cerca del municipio: Almonaster");
+			  alert("Compruebe que tiene activada la ubicación del dispositivo y que se encuentra cerca del municipio: Almonaster la Real");
 			  loading(false);
 		  };
 		  loading(true);	
